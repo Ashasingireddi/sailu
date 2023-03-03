0bf40d6921541c43212a2230fe059d5aec95b089
Ashasingireddi <ashaashaec034@gmail.com>
2023-03-03 19:44:04 +0530

    azfcvxshys

---
+++ springzimbramail/src/main/java/com/SpringzimbramailApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringzimbramailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringzimbramailApplication.class, args);
+	}
+
+}
